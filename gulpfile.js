730a99b2adf46d908420aa77407747571d6d4a02
boeckMt <bbmatdd@gmail.com>
2016-03-12 17:55:46 +0100

    refactor: make proxyserver a own module

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,4 @@
 var gulp = require('gulp');
-var express = require('express');
-var request = require('request');
-var bodyParser = require('body-parser');
-var _open = require('open');
-gulp.task('proxyserver', function () {
-    var port = 9005;
-    var app = express();
-    app.use(express.static(__dirname));
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    app.use(bodyParser.json({
-        type: 'application/json'
-    }));
-    app.post('/proxy', function (req, res) {
-        var proxyUrl = req.body.proxy;
-        request.get({
-            //timeout: 1000,
-            rejectUnauthorized: false,
-            url: proxyUrl
-        }, function (error, response, body) {
-            if (error) {
-                return console.error('proxy failed:', error);
-            }
-            if (response.headers['content-type'].lastIndexOf('text/html') >= 0) {
-                res.status(406).json({ error: 'The requested resource is only capable of generating content not acceptable according to the Accept headers sent in the request' });
-            }
-            else {
-                res.set('Content-Type', response.headers['content-type']);
-                res.send(body);
-            }
-        });
-    });
-    var server = app.listen(port, function () {
-        var host = server.address().address;
-        var port = server.address().port;
-        console.log('Example app listening at http://%s:%s', host, port);
-    });
-    _open("http://localhost:" + port + "/src/index.html#/wms", "firefox");
-});
 //------------------------------------------------------------------------------
 var concat = require('gulp-concat');
 gulp.task('concat', function () {
---
+++ proxyserver/index.ts
@@ -0,0 +1,46 @@
+//express proxy and static files -server for WMS viewer
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var _open = require('open');
+
+var port = 9005;
+var app = express();
+
+app.use(express.static(`./`));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json({
+  type: 'application/json'
+}));
+
+
+app.post('/proxy', (req, res) => {
+  var proxyUrl = req.body.proxy;
+  request.get({
+    //timeout: 1000,
+    rejectUnauthorized: false,
+    url: proxyUrl
+  }, (error, response, body) => {
+      if (error) {
+        return console.error('proxy failed:', error);
+      }
+
+      if (response.headers['content-type'].lastIndexOf('text/html') >= 0) {
+        res.status(406).json({ error: 'The requested resource is only capable of generating content not acceptable according to the Accept headers sent in the request' })
+      } else {
+        res.set('Content-Type', response.headers['content-type']);
+        res.send(body);
+      }
+
+    });
+});
+
+var server = app.listen(port, () => {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('proxy server for wms-app listening at http://%s:%s', host, port);
+})
+
+_open(`http://localhost:${port}/src/index.html#/wms`, "firefox");
