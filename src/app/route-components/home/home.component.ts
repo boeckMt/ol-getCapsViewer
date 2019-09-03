f06bf3954c20e3c0aec95329398e723dc887fd2d
boec_mt <Mathias.Boeck@dlr.de>
2019-09-03 09:22:07 +0200

    update style and layout

--- src/app/route-components/home/home.component.ts
+++ src/app/route-components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { AppStoreService } from '../../shared/app-store.service';
 import * as wms from '../../../../xmlns/www.opengis.net/wms';
 import { Subscription } from 'rxjs';
@@ -6,10 +6,10 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  host: { 'class': 'content-container' } //to add class to the component for clarity container height
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  @HostBinding('class') class = 'content-container';
   caps: wms.WMS_CapabilitiesType;
   service: wms.ServiceType;
   capability: wms.CapabilityType;
